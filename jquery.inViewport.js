1d5371b628eeed5174b1aac59ee849950c504bb8
Edward Casbon <edward@edwardcasbon.co.uk>
2014-04-09 12:31:44 +0100

    Improved accuracy of element positioning.

--- jquery.inViewport.js
+++ jquery.inViewport.js
@@ -3,8 +3,8 @@
 * Author: Edward Casbon
 * Email: edward@edwardcasbon.co.uk
 * URL: http://www.edwardcasbon.co.uk
-* Version: 1.0
-* Date: 26th March 2013
+* Version: 1.1
+* Date: 9th April 2014
 *
 * Example usage:
 * $("#elementID").inViewport(function(){
@@ -15,76 +15,69 @@
 * Argument 1 is a callback function for when the element appears in the viewport.
 * Argument 2 is a callback function for when the element disappears from the viewport.
 **/
-
-(function($) {
+(function($){
 	
 	// Global array where all the elements are recorded.
 	var inViewportElements = [];
 	
 	// Plugin starts here.
 	$.fn.inViewport = function(inFunction, outFunction) {
-
+		
 		// Loop through all the elements selected.
 		return this.each(function(){
 			
-			// Cache the element.
-			$element = $(this);
-
-			// Get the position of the element.
-			elTop = $element.offset().top;
-			elLeft = $element.offset().left;
-			elRight = elLeft + $element.outerWidth();
-			elBottom = elTop + $element.outerHeight();
+			// Add the element to the elements array.
+			inViewportElements.push(this);
 			
-			// Create an object and add it to the elements array.
-			elObject = {
-				'element': 		this,
-				'elTop': 		elTop,
-				'elLeft': 		elLeft,
-				'elRight': 		elRight,
-				'elBottom': 		elBottom,
-				'inFunction': 		inFunction,
-				'outFunction': 		outFunction,
-				'currentStatus': 	'out'
-			};
-			inViewportElements.push(elObject);
-
-			// Function to check if an element is in the viewport.
-			inViewport = function(el) {
-				viewPortTop = $(window).scrollTop();
-				viewPortBottom = viewPortTop + $(window).height();
-				viewPortLeft = $(window).scrollLeft();
-				viewPortRight = viewPortLeft + $(window).width();
-								
-				return ( 
-					el.elTop < viewPortBottom 
-					&& el.elLeft < viewPortRight 
-					&& el.elBottom > viewPortTop 
-					&& el.elRight > viewPortLeft
-				)
-			}
-
 			// Do the check to see if any of the elements are in the viewport.
 			doCheck = function() {
-				for(i=0; i<inViewportElements.length; i++) {
-					theElement = inViewportElements[i];
-					if (inViewport(theElement) && theElement.currentStatus != 'in') {
-						// Element is in the viewport.
-						theElement.currentStatus = 'in';
-						if(typeof(theElement.inFunction) == "function") theElement.inFunction.call(theElement.element);
-					} else if(!inViewport(theElement) && theElement.currentStatus != 'out') {
-						// Element has left the viewport.
-						theElement.currentStatus = 'out';
-						if(typeof(theElement.outFunction) == "function") theElement.outFunction.call(theElement.element);
+
+				// Calculate the viewports measurements.
+				var vpTop 		= $(window).scrollTop(),
+					vpBottom 	= vpTop + $(window).height(),
+					vpLeft 		= $(window).scrollLeft(),
+					vpRight 	= vpLeft + $(window).width();
+				
+				// Loop through the elements checking if they're in the viewport.
+				for(var i=0; i<inViewportElements.length; i++) {
+					
+					// Calculate the elements measurements.
+					var element 	= inViewportElements[i],
+						$element 	= $(element),
+						elTop 		= $element.offset().top,
+						elLeft 		= $element.offset().left,
+						elRight 	= elLeft + $element.outerWidth(),
+						elBottom  	= elTop + $element.outerHeight();
+					
+					// Check the elements measurements against the viewports.	
+					if (
+						elTop < vpBottom 
+						&& elLeft < vpRight 
+						&& elBottom > vpTop 
+						&& elRight > vpLeft
+					) {
+						// In viewport.
+						if($element.attr('data-vpStatus') != "in") {
+							// If not already in the viewport, then update the status...
+							$element.attr('data-vpStatus', "in");
+							// .. and fire the callback.
+							if(typeof(inFunction) == "function") inFunction.call(element);
+						}
+					} else {
+						// Not in viewport.
+						if($element.attr('data-vpStatus') != "out") {
+							// If not already out of the viewport, then update the status...
+							$element.attr('data-vpStatus', "out");
+							// .. and fire the callback.
+							if(typeof(outFunction) == "function") outFunction.call(element);
+						}
 					}
 				}
-			}
-
-			// On scroll and resize, check the elements viewport status.
-			$(window).scroll(doCheck).resize(doCheck);
+			};
 			
-			// On load, check the elements viewport status.
-			doCheck();
+			// On scroll, resize and load, check if the elements are in the viewport.
+			$(window).scroll(doCheck).resize(doCheck);
+			doCheck();			
 		});
-	};
+	}
 })(jQuery);
